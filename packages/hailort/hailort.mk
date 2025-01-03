ee132243e179ab2279c5358d0c9910e56e5cfc21
Cocoa <i@uwucocoa.moe>
2025-01-03 16:12:01 +0100

    build spdlog bundled with hailo
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- packages/hailort/hailort.mk
+++ packages/hailort/hailort.mk
@@ -12,6 +12,6 @@ HAILORT_LICENSE = MIT
 HAILORT_LICENSE_FILES = hailort/LICENSE
 HAILORT_SUPPORTS_IN_SOURCE_BUILD = NO
 
-HAILORT_DEPENDENCIES = spdlog protobuf eigen
+HAILORT_DEPENDENCIES = protobuf eigen
 
 $(eval $(cmake-package))
